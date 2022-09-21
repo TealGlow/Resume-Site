1b2bf8f716134e5614c226142c366af0831a2ee1
TealGlow <alyssacomstock95@gmail.com>
2022-09-20 19:27:03 -0700

    removed unused code and functions

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-//const mongoose = require("mongoose");
-const sentencer = require("sentencer");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const words=[];
 
 //setting up the app
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
-//setting up the blog DB
-//mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
-
-//setting up the post schema
-/*
-const postSchema={
-  title: String,
-  content: String
-};*/
-
-//model for the posts hello :)
-//const Post = mongoose.model("Post", postSchema);
 
 //setting up the app
 app.use(express.static("public"));
@@ -44,70 +24,6 @@ app.get("/resume", (req,res)=>{
   res.render("resume");
 });
 
-//pages for the blogging site
-/*
-app.get("/blog", (req, res)=>{
-  //blog screen
-  Post.find({}, function(err, posts){
-    res.render("blog", {
-      startingContent: homeStartingContent,
-      posts: posts
-    });
-  });
-});
-
-
-app.get("/compose", (req,res)=>{
-  //compose allowfullscreen
-  res.render("compose");
-});
-
-/
-app.post("/compose", (req, res)=>{
-  //handing the entering of post
-  const post = new Post({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  });
-
-  post.save(function(err){
-    if(!err){
-      res.redirect("/blog");
-    }
-  });
-});
-
-
-app.get("/posts/:postId", function(req, res){
-  const reqPostId = req.params.postId;
-  //const requestedTitle = _.lowerCase(req.params.postName);
-
-  Post.findOne({_id: reqPostId}, function(err, post){
-    if(!err){
-      res.render("post",{title: post.title, content: post.content});
-    }
-  });
-});
-*/
-//pages for word list
-app.get("/adjnoun", (req, res)=>{
-  res.render("randomwords", {items: words});
-});
-
-app.post("/adjnoun", (req,res)=>{
-  for(var i = 0; i<10; i++){
-    //populate word list
-    words.push(sentencer.make("{{ adjective }} {{noun}}"));
-  }
-  if(words){
-    //send those words over to the page
-    res.render("randomwords", {items: words});
-    while(words.length>0){
-      //delete words right after
-      words.pop();
-    }
-  }
-});
 
 
 //app listening on PORT
